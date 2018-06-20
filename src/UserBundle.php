7eec6192eed9760133ab1cefb8953433be7901f7
gbtux <gu.bordes@gmail.com>
2018-06-20 22:31:10 +0200

    initial import

---
+++ src/UserBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gbtux
+ * Date: 20/06/18
+ * Time: 22:20
+ */
+
+namespace Gbtux\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class UserBundle extends Bundle
+{
+
+}
